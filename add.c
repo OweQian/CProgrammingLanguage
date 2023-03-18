ead07268690be4b7a6b55ae42a6d58247ca76726
qian wang <1510106069@qq.com>
2023-03-19 01:38:22 +0800

    feat: 加法例题

--- add.c
+++ add.c
@@ -1,7 +1,40 @@
-#include <stdio.h>
-int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d\n", a + b);
-    return 0;
+// #include <stdio.h>
+
+// int main() {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     printf("%d\n", a + b);
+//     return 0;
+// }
+
+// int main() {
+//     int a, b, t;
+//     scanf("%d", &t);
+//     while(t--) {
+//         scanf("%d %d", &a, &b);
+//         printf("%d\n", a + b);
+//     }
+//     return 0;
+// }
+
+// int main() {
+//     int a, b;
+//     while(scanf("%d %d", &a, &b) != 'EOF') {
+//         printf("%d\n", a + b);
+//     }
+//     return 0;
+// }
+
+// int main() {
+//     int a, b;
+//     while(scanf("%d %d", &a, &b) != 'EOF') {
+//         if (a == 0 && b == 0)
+//             break;
+//         printf("%d\n", a + b);
+//     }
+//     return 0;
+// }
+
+int add(int a, int b) {
+    return a + b;
 }
