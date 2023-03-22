fbdb9ae29b7db99665ca94304f203332d10fe8af
OweQian <1510106069@qq.com>
2023-03-22 17:10:11 +0800

    feat: 变量交换

---
+++ exchangeVariable.c
@@ -0,0 +1,34 @@
+// #include <stdio.h>
+
+// int main() {
+//     int a, b, temp;
+//     while (scanf("%d %d", &a, &b) != EOF) {
+//         temp = a;
+//         a = b;
+//         b = temp;
+//         printf("%d %d\n", a, b);
+//     }
+//     return 0;
+// }
+
+// int main() {
+//     int a, b;
+//     while (scanf("%d %d", &a, &b) != EOF) {
+//         a = a + b;
+//         b = a - b;
+//         a = a - b;
+//         printf("%d %d\n", a, b);
+//     }
+//     return 0;
+// }
+
+// int main() {
+//     int a, b;
+//     while (scanf("%d %d", &a, &b) != EOF) {
+//         a = a ^ b;
+//         b = a ^ b;
+//         a = a ^ b;
+//         printf("%d %d\n", a, b);
+//     }
+//     return 0;
+// }
