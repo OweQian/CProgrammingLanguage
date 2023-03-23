f693b00d55cbce8f9ba890f4f28d32b7ff588e72
OweQian <1510106069@qq.com>
2023-03-23 11:47:45 +0800

    feat: 条件运算符

---
+++ processControl.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// int main() {
+//     int a, b, t;
+//     scanf("%d", &t);
+//     while (t--) {
+//         scanf("%d %d", &a, &b);
+//         if (b == 0 || a % b) {
+//             printf("NO\n");
+//         } else {
+//             printf("YES\n");
+//         }
+//     }
+//     return 0;
+// }
+
+// int main() {
+//     int a, b, t;
+//     scanf("%d", &t);
+//     while (t--) {
+//         scanf("%d %d", &a, &b);
+//         printf("%s\n", b == 0 || a % b ? "NO" : "YES");
+//     }
+//     return 0;
+// }
