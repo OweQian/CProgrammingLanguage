2dedff5ace53a8c4876491d7b19072803b7e153d
OweQian <1510106069@qq.com>
2023-03-31 10:46:42 +0800

    feat: 整数溢出

---
+++ integerOverflow.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+typedef unsigned long long ull;
+const ull MAX = ((ull)1)<<62;
+
+int main() {
+    int t;
+    ull a, b, c, d;
+    scanf("%d", &t);
+    while(t--) {
+        scanf("%llu %llu %llu %llu", &a, &b, &c, &d);
+        if (a == MAX && b == MAX && c == MAX && d == MAX)
+            printf("18446744073709551616\n");
+        else    
+            printf("%llu\n", a + b + c + d);
+
+    }
+    return 0;
+}
