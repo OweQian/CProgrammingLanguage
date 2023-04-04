589fcec08f9713971b515af5f593b3d4c4b44805
OweQian <1510106069@qq.com>
2023-04-04 15:35:04 +0800

    feat: 三数大小

---
+++ threeNums.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void swap(int* x, int* y) {
+    int tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
+int main() {
+    int a, b, c;
+    while(scanf("%d %d %d", &a, &b, &c) != EOF) {
+        if (a > b) swap(&a, &b);
+        if (b > c) swap(&b, &c);
+        if (a > b) swap(&a, &b);
+        printf("%d %d %d\n", a, b, c);
+    }
+    return 0;
+}
