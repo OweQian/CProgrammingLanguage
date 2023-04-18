0bfae2501af29c540ee4e001925d5de1d8eedc18
OweQian <1510106069@qq.com>
2023-04-18 15:31:50 +0800

    feat: 最大公约数

---
+++ gcd.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int gcd (int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+int main() {
+    int a, b;
+    while(1) {
+        if (scanf("%d%d", &a, &b) != EOF) {
+            printf("%d\n", gcd(a, b));
+        }
+    }
+    return 0;
+}
