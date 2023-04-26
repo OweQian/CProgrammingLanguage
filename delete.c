622155cfc9c577631adc4832eadc7609371804d5
OweQian <1510106069@qq.com>
2023-04-26 11:15:36 +0800

    feat: 数组删除

---
+++ delete.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int n;
+int a[10000001], x;
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int i, x;
+    while (scanf("%d", &n) != EOF) {
+        for (i = 0; i < n; ++i) {
+            scanf("%d", &a[i]);
+        }
+        for (i = 1; i < n; ++i) {
+            a[i - 1] = a[i];
+        }
+        for (i = 0; i < n - 1; ++i) {
+            if (i) {
+                printf(" ");
+            }
+            printf("%d", a[i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
