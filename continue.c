c3de1001aea808c44d0b42f83b2cd99a1b5a1f2f
OweQian <1510106069@qq.com>
2023-06-07 10:55:21 +0800

    feat: continue

---
+++ continue.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, a[1001];
+    int target;
+    int i;
+    while (scanf("%d", &n) != EOF) {
+        for (i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+        }
+        scanf("%d", &target);
+        for (i = 0; i < n; i++) {
+            if (a[i] != target)
+                continue;
+            else
+                break; 
+        }
+        printf("%s\n", i < n ? "yes" : "no");
+    }
+    
+    return 0;
+}
